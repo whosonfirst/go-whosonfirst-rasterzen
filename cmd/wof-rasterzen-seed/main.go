9dc686e7fd64b2f7fa82adee2a27b3749aaec590
thisisaaronland <devnull@localhost>
2019-08-30 18:23:15 -0700

    refactor to gather tiles per place record

--- cmd/wof-rasterzen-seed/main.go
+++ cmd/wof-rasterzen-seed/main.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"errors"
 	"flag"
-	"fmt"
+	_ "fmt"
+	"github.com/aaronland/go-string/dsn"	
 	"github.com/go-spatial/geom"
 	"github.com/go-spatial/geom/slippy"
 	"github.com/jtacoma/uritemplates"
@@ -22,6 +23,7 @@ import (
 	"github.com/whosonfirst/go-whosonfirst-log"
 	"github.com/whosonfirst/warning"
 	"io"
+	"io/ioutil"	
 	"os"
 	"strings"
 )
@@ -33,131 +35,6 @@ type WOFGatherTilesOptions struct {
 	Include flags.KeyValueArgs
 	Exclude flags.KeyValueArgs
 	Paths   []string
-}
-
-func WOFGatherTilesFunc(opts *WOFGatherTilesOptions) (seed.GatherTilesFunc, error) {
-
-	gather_func := func(ctx context.Context, tileset *seed.TileSet) error {
-
-		index_func := func(fh io.Reader, ctx context.Context, args ...interface{}) error {
-
-			select {
-			case <-ctx.Done():
-				return nil
-			default:
-				// pass
-			}
-
-			principal, err := utils.IsPrincipalWOFRecord(fh, ctx)
-
-			if err != nil {
-				return err
-			}
-
-			if !principal {
-				return nil
-			}
-
-			f, err := feature.LoadFeatureFromReader(fh)
-
-			if err != nil {
-
-				if !warning.IsWarning(err) {
-					return err
-				}
-
-				tileset.Logger.Warning(err)
-			}
-
-			for _, e := range opts.Exclude {
-
-				path := e.Key
-				test := e.Value
-
-				rsp := gjson.GetBytes(f.Bytes(), path)
-
-				if rsp.Exists() && rsp.String() == test {
-					tileset.Logger.Debug("%s (%s) is being exluded because it matches the %s=%s -exclude test", f.Name(), f.Id(), path, test)
-					return nil
-				}
-			}
-
-			if len(opts.Include) > 0 {
-
-				include_ok := false
-
-				for _, i := range opts.Include {
-
-					path := i.Key
-					test := i.Value
-
-					rsp := gjson.GetBytes(f.Bytes(), path)
-
-					if !rsp.Exists() {
-						tileset.Logger.Debug("%s (%s) fails -include test because %s does not exist", f.Name(), f.Id(), path)
-						continue
-					}
-
-					if rsp.String() != test {
-						tileset.Logger.Debug("%s (%s) fails -include test because %s != %s (is %s)", f.Name(), f.Id(), path, test, rsp.String())
-						continue
-					}
-
-					include_ok = true
-					break
-				}
-
-				if !include_ok {
-					tileset.Logger.Debug("%s (%s) is being excluded because all -include tests failed", f.Name(), f.Id())
-					return nil
-				}
-			}
-
-			bboxes, err := f.BoundingBoxes()
-
-			if err != nil {
-				return err
-			}
-
-			tileset.Logger.Status("Seed tiles for %s", f.Name())
-
-			for _, bounds := range bboxes.Bounds() {
-
-				min := [2]float64{bounds.Min.X, bounds.Min.Y}
-				max := [2]float64{bounds.Max.Y, bounds.Max.Y}
-
-				ex := geom.NewExtent(min, max)
-
-				for z := opts.MinZoom; z < opts.MaxZoom; z++ {
-
-					for _, t := range slippy.FromBounds(ex, uint(z)) {
-						tileset.AddTile(t)
-					}
-				}
-			}
-
-			return nil
-		}
-
-		idx, err := index.NewIndexer(opts.Mode, index_func)
-
-		if err != nil {
-			return err
-		}
-
-		for _, path := range opts.Paths {
-
-			err := idx.IndexPath(path)
-
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}
-
-	return gather_func, nil
 }
 
 func main() {
@@ -387,56 +264,179 @@ func main() {
 	seeder.SeedSVG = *seed_svg
 	seeder.SeedPNG = *seed_png
 
-	tileset, err := seed.NewTileSetFromDSN(*seed_tileset_catalog_dsn)
+	index_func := func(fh io.Reader, ctx context.Context, args ...interface{}) error {
 
-	if err != nil {
-		logger.Fatal(err)
-	}
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			// pass
+		}
+		
+		principal, err := utils.IsPrincipalWOFRecord(fh, ctx)
+		
+		if err != nil {
+			return err
+		}
+		
+		if !principal {
+			return nil
+		}
+		
+		f, err := feature.LoadFeatureFromReader(fh)
+		
+		if err != nil {
 
-	tileset.Logger = logger
-	tileset.Timings = *timings
-
-	paths := flag.Args()
-
-	gather_opts := &WOFGatherTilesOptions{
-		Mode:    *mode,
-		MinZoom: *min_zoom,
-		MaxZoom: *max_zoom,
-		Include: include,
-		Exclude: exclude,
-		Paths:   paths,
-	}
-
-	gather_func, err := WOFGatherTilesFunc(gather_opts)
-
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	err = seed.GatherTiles(tileset, seeder, gather_func)
-
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	if *count {
-		fmt.Println(tileset.Count())
-		os.Exit(0)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	ok, errors := seeder.SeedTileSet(ctx, tileset)
-
-	if !ok {
-
-		for _, e := range errors {
-			logger.Error(e)
+			if !warning.IsWarning(err) {
+				return err
+			}
+			
+			logger.Warning(err)
+		}
+		
+		for _, e := range exclude {
+			
+			path := e.Key
+			test := e.Value
+			
+			rsp := gjson.GetBytes(f.Bytes(), path)
+			
+			if rsp.Exists() && rsp.String() == test {
+				logger.Debug("%s (%s) is being exluded because it matches the %s=%s -exclude test", f.Name(), f.Id(), path, test)
+				return nil
+			}
+		}
+		
+		if len(include) > 0 {
+			
+			include_ok := false
+			
+			for _, i := range include {
+				
+				path := i.Key
+				test := i.Value
+				
+				rsp := gjson.GetBytes(f.Bytes(), path)
+				
+				if !rsp.Exists() {
+					logger.Debug("%s (%s) fails -include test because %s does not exist", f.Name(), f.Id(), path)
+					continue
+				}
+				
+				if rsp.String() != test {
+					logger.Debug("%s (%s) fails -include test because %s != %s (is %s)", f.Name(), f.Id(), path, test, rsp.String())
+					continue
+				}
+				
+				include_ok = true
+				break
+			}
+			
+			if !include_ok {
+				logger.Debug("%s (%s) is being excluded because all -include tests failed", f.Name(), f.Id())
+				return nil
+			}
+		}
+		
+		bboxes, err := f.BoundingBoxes()
+		
+		if err != nil {
+			return err
 		}
 
-		logger.Fatal("tile seeding completed unsuccessfully")
-	}
+		// move this in to a function...
 
+		dsn_str := *seed_tileset_catalog_dsn
+		dsn_map, err := dsn.StringToDSNWithKeys(dsn_str, "catalog")		
+
+		if err != nil {
+			return err
+		}
+
+		if strings.ToUpper(dsn_map["catalog"]) == "SQLITE" {
+
+			tmpfile, err := ioutil.TempFile("", "rasterzen")
+
+			if err != nil {
+				return err
+			}
+
+			err = tmpfile.Close()
+
+			if err != nil {
+				return err
+			}
+
+			dsn_map["dsn"] = tmpfile.Name()
+			dsn_str = dsn_map.String()
+
+			defer os.Remove(tmpfile.Name())
+		}
+
+		tileset, err := seed.NewTileSetFromDSN(dsn_str)
+		
+		if err != nil {
+			return err
+		}
+
+		tileset.Logger = logger
+		tileset.Timings = *timings
+		
+		tileset.Logger.Status("Seed tiles for %s", f.Name())
+
+		// just use seed.GatherTilesFuncExtent...
+		
+		for _, bounds := range bboxes.Bounds() {
+			
+			min := [2]float64{bounds.Min.X, bounds.Min.Y}
+			max := [2]float64{bounds.Max.Y, bounds.Max.Y}
+			
+			ex := geom.NewExtent(min, max)
+			
+			for z := *min_zoom; z < *max_zoom; z++ {
+				
+				for _, t := range slippy.FromBounds(ex, uint(z)) {
+					tileset.AddTile(t)
+				}
+			}
+		}
+
+		if *count {
+			tileset.Logger.Status("Tile count for %s (%s): %d", f.Name(), f.Id(), tileset.Count())
+			return nil
+		}
+
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		ok, errors := seeder.SeedTileSet(ctx, tileset)
+
+		if !ok {
+
+			tileset.Logger.Error("Failed to seeding tileset for %s", f.Name())
+			
+			for _, e := range errors {
+				tileset.Logger.Error(e)
+			}		
+		}
+		
+		return nil
+	}
+	
+	idx, err := index.NewIndexer(*mode, index_func)
+	
+	if err != nil {
+		logger.Fatal(err)
+	}
+	
+	for _, path := range flag.Args() {
+
+		err := idx.IndexPath(path)
+		
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+	
 	os.Exit(0)
 }
